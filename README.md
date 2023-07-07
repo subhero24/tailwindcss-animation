e11d8fe41e3bec9e842f0e96debcd751aba33714
SubHero <github@bvdsoftware.be>
2023-07-07 13:34:05 +0200

    Allow complex custom animations

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+This plugin gives you tailwind utility classes for all animation properties
+
 # Install
 
 ```
@@ -14,14 +16,44 @@ export default {
 };
 ```
 
-# Usage
+This plugin uses explicit transition and animation prefixes for classes, so feel free to disabled the core plugins:
+
+```javascript
+export default {
+	corePlugins: {
+		transitionDelay: false,
+		transitionDuration: false,
+		transitionTimingFunction: false,
+	},
+};
+```
+
+# Classes
 
 ## Animation
 
+### animation
+
+theme.animation
+
+.animation-spin
+.animation-ping
+.animation-pulse
+.animation-bounce
+
+### animation name
+
+theme.keyframes
+
+.animation-name-spin
+.animation-name-ping
+.animation-name-pulse
+.animation-name-bounce
+
 ### animation play state
 
-.paused
-.running
+.play-state-paused
+.play-state-running
 
 ### animation fill mode
 
@@ -44,9 +76,13 @@ theme.animationIterations
 .iterations-1
 .iterations-infinite
 
-### animation duration
+## Duration
 
+theme.duration
 theme.animationDuration
+theme.transitionDuration
+
+### animation duration
 
 .animation-duration-75
 .animation-duration-100
@@ -57,8 +93,33 @@ theme.animationDuration
 .animation-duration-700
 .animation-duration-1000
 
+### transition duration
+
+.transition-duration-75
+.transition-duration-100
+.transition-duration-150
+.transition-duration-200
+.transition-duration-300
+.transition-duration-500
+.transition-duration-700
+.transition-duration-1000
+
+## Delay
+
+theme.delay
+
+.delay-75
+.delay-100
+.delay-150
+.delay-200
+.delay-300
+.delay-500
+.delay-700
+.delay-1000
+
 ### animation delay
 
+theme.delay
 theme.animationDelay
 
 .animation-delay-75
@@ -70,54 +131,9 @@ theme.animationDelay
 .animation-delay-700
 .animation-delay-1000
 
-### animation timing function
-
-theme.animationTimingFunction
-
-.animation-ease-linear
-.animation-ease-in
-.animation-ease-out
-.animation-ease-in-out
-
-### animation name
-
-theme.keyframes
-
-.animation-spin
-.animation-ping
-.animation-pulse
-.animation-bounce
-
-## Transitions
-
-It also generates the default TailwindCSS classes with a transition prefix to disambiguate between transitions and animations.
-Feel free to disabled the core plugins:
-
-```javascript
-export default {
-	corePlugins: {
-		transitionDelay: false,
-		transitionDuration: false,
-		transitionTimingFunction: false,
-	},
-};
-```
-
-### transition duration
-
-theme.transitionDuration
-
-.transition-duration-75
-.transition-duration-100
-.transition-duration-150
-.transition-duration-200
-.transition-duration-300
-.transition-duration-500
-.transition-duration-700
-.transition-duration-1000
-
 ### transition delay
 
+theme.delay
 theme.transitionDelay
 
 .transition-delay-75
@@ -129,11 +145,79 @@ theme.transitionDelay
 .transition-delay-700
 .transition-delay-1000
 
+## Timing function
+
+theme.timingFunction
+
+.ease-linear
+.ease-in
+.ease-out
+.ease-in-out
+
+### animation timing function
+
+theme.timingFunction
+theme.animationTimingFunction
+
+.animation-ease-linear
+.animation-ease-in
+.animation-ease-out
+.animation-ease-in-out
+
 ### transition timing function
 
+theme.timingFunction
 theme.transitionTimingFunction
 
 .transition-ease-linear
 .transition-ease-in
 .transition-ease-out
 .transition-ease-in-out
+
+# Advanced usage
+
+## Use your timing functions in the animation values
+
+```javascript
+theme: {
+	extend: {
+		keyframes: {
+			'fade-in': {
+				'0%': { opacity: 0 },
+				'100%': { opacity: 1 },
+			},
+			'slide-up': {
+				'0%': { transform: 'translateY(2rem)' },
+				'100%': { transform: 'translateY(0)' },
+			}
+		}
+		animation: {
+			appear: 'fade-in 200ms ease-out, slide-up 1s wobbly'
+		},
+		animationTimingFunction: {
+			wobbly: 'linear(0, 0.037 1.8%, 0.151 3.9%, 0.767 11.2%, 0.99, 1.131 17.5%, 1.174 19%, 1.199 20.7%, 1.208, 1.193 25.1%, 1.011 36.1%, 0.976, 0.959 42.8%, 0.96 47.5%, 0.997 58.3%, 1.008 64.6%, 0.999 85.5% 100%)'
+		},
+	}
+}
+```
+
+## Allow complex custom animations/transitions with different easings, durations, delays, ...
+
+```html
+<div class="transition-[opacity,transform] transition-ease-[out,wobbly] transition-duration-[200ms,1s]"></div>
+```
+
+## Specify fallback timing functions
+
+```javascript
+theme: {
+	extend: {
+		timingFunction: {
+			wobbly: [
+				'ease-out', // Fallback if linear is not supported
+				'linear(0, 0.037 1.8%, 0.151 3.9%, 0.767 11.2%, 0.99, 1.131 17.5%, 1.174 19%, 1.199 20.7%, 1.208, 1.193 25.1%, 1.011 36.1%, 0.976, 0.959 42.8%, 0.96 47.5%, 0.997 58.3%, 1.008 64.6%, 0.999 85.5% 100%)',
+			]
+		},
+	}
+}
+```
--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 let plugin = require("tailwindcss/plugin");
 
-const COMMA = /\,(?![^(]*\))/g;
+const DIRECTIONS = new Set(["normal", "reverse", "alternate", "alternate-reverse"]);
+const PLAY_STATES = new Set(["running", "paused"]);
+const FILL_MODES = new Set(["none", "forwards", "backwards", "both"]);
+const ITERATION_COUNTS = new Set(["infinite"]);
+const TIMINGS = new Set(["linear", "ease", "ease-in", "ease-out", "ease-in-out", "step-start", "step-end"]);
+const TIMING_FNS = ["cubic-bezier", "steps", "linear"];
+
+const COMMA = /\,(?![^(]*\))/g; // Comma separator that is not located between brackets. E.g.: `cubiz-bezier(a, b, c)` these don't count.
+const SPACE = /\ +(?![^(]*\))/g; // Similar to the one above, but with spaces instead.
+const TIME = /^(-?[\d.]+m?s)$/;
+const DIGIT = /^(\d+)$/;
 
 let defaultOptions = {
 	theme: {
@@ -53,7 +63,7 @@ let defaultOptions = {
 			700: "700ms",
 			1000: "1000ms",
 		},
-		animationTimingFunction: {
+		timingFunction: {
 			"DEFAULT": "cubic-bezier(0.4, 0, 0.2, 1)",
 			"linear": "linear",
 			"in": "cubic-bezier(0.4, 0, 1, 1)",
@@ -73,6 +83,14 @@ function animationPlugin({ addUtilities, matchUtilities, theme, config, variants
 			return [key, { [`@keyframes ${prefix(key)}`]: value }];
 		})
 	);
+
+	let animationDelay = { ...theme("delay"), ...theme("animationDelay") };
+	let animationDuration = { ...theme("duration"), ...theme("animationDuration") };
+	let animationTimingFunction = { ...theme("timingFunction"), ...theme("animationTimingFunction") };
+
+	let transitionDelay = { ...theme("delay"), ...theme("transitionDelay") };
+	let transitionDuration = { ...theme("duration"), ...theme("transitionDuration") };
+	let transitionTimingFunction = { ...theme("timingFunction"), ...theme("transitionTimingFunction") };
 
 	matchUtilities({ "fill-mode": (value) => ({ animationFillMode: value }) }, { values: theme("animationFillMode") });
 
@@ -97,51 +115,214 @@ function animationPlugin({ addUtilities, matchUtilities, theme, config, variants
 	);
 
 	matchUtilities(
-		{ "animation-duration": (value) => ({ animationDuration: value }) },
-		{ values: theme("animationDuration") }
+		{
+			duration: (value) => {
+				({ animationDuration: value, transitionDuration: value });
+			},
+		},
+		{
+			values: theme("duration"),
+		}
 	);
+
+	matchUtilities({ "animation-duration": (value) => ({ animationDuration: value }) }, { values: animationDuration });
 
 	matchUtilities(
 		{ "transition-duration": (value) => ({ transitionDuration: value }) },
-		{ values: theme("transitionDuration") }
+		{ values: transitionDuration }
 	);
 
-	matchUtilities({ "animation-delay": (value) => ({ animationDelay: value }) }, { values: theme("animationDelay") });
+	matchUtilities(
+		{
+			delay: (value) => {
+				({ animationDelay: value, transitionDelay: value });
+			},
+		},
+		{
+			values: theme("delay"),
+		}
+	);
+
+	matchUtilities({ "animation-delay": (value) => ({ animationDelay: value }) }, { values: animationDelay });
+
+	matchUtilities({ "transition-delay": (value) => ({ transitionDelay: value }) }, { values: transitionDelay });
 
 	matchUtilities(
-		{ "transition-delay": (value) => ({ transitionDelay: value }) },
-		{ values: theme("transitionDelay") }
+		{
+			ease: (value) => {
+				if (typeof value === "string") {
+					let values = value.split(COMMA).map((value) => animationTimingFunction[value] ?? value);
+					let arrays = enumerateArrays(values);
+
+					return { animationTimingFunction: arrays, transitionTimingFunction: arrays };
+				} else {
+					return { animationTimingFunction: value, transitionTimingFunction: value };
+				}
+			},
+		},
+		{ values: theme("timingFunction") }
 	);
 
 	matchUtilities(
 		{
 			"animation-ease": (value) => {
 				if (typeof value === "string") {
-					value = value.split(COMMA).map((value) => theme("animationTimingFunction")[value] ?? value);
+					let values = value.split(COMMA).map((value) => animationTimingFunction[value] ?? value);
+					let arrays = enumerateArrays(values);
+
+					return { animationTimingFunction: arrays };
+				} else {
+					return { animationTimingFunction: value };
 				}
-				return { animationTimingFunction: value };
 			},
 		},
-		{ values: theme("animationTimingFunction") }
+		{ values: animationTimingFunction }
 	);
 
 	matchUtilities(
 		{
 			"transition-ease": (value) => {
 				if (typeof value === "string") {
-					value = value.split(COMMA).map((value) => theme("transitionTimingFunction")[value] ?? value);
-				}
+					let values = value.split(COMMA).map((value) => transitionTimingFunction[value] ?? value);
+					let arrays = enumerateArrays(values);
 
-				return { transitionTimingFunction: value };
+					return { transitionTimingFunction: arrays };
+				} else {
+					return { transitionTimingFunction: value };
+				}
 			},
 		},
-		{ values: theme("transitionTimingFunction") }
+		{ values: transitionTimingFunction }
 	);
 
 	matchUtilities(
-		{ animation: (value) => [keyframes[value], { animationName: prefix(value) }] },
+		{
+			animationName: (value) => {
+				let values = value.split(COMMA);
+				let frames = values.map((value) => keyframes[value]);
+
+				return [...frames, { animationName: values.map((value) => prefix(value)).join() }];
+			},
+		},
 		{ values: Object.fromEntries(Object.keys(theme("keyframes")).map((key) => [key, key])) }
+	);
+
+	matchUtilities(
+		{
+			animation: (value) => {
+				let timings = Object.keys(animationTimingFunction);
+				let animations = parseAnimationValue(value, timings);
+
+				function replaceTimingFunction(value, name) {
+					let timing = animationTimingFunction[name];
+					if (timing) {
+						if (timing instanceof Array) {
+							return timing.map((timing) => value.replace(name, timing));
+						} else {
+							return value.replace(name, timing);
+						}
+					} else {
+						return value;
+					}
+				}
+
+				function replaceAnimationName(value, name) {
+					if (name && keyframes[name]) {
+						if (value instanceof Array) {
+							return value.map((value) => value.replace(name, prefix(name)));
+						} else {
+							return value.replace(name, prefix(name));
+						}
+					} else {
+						return value;
+					}
+				}
+
+				let results = animations.map(({ name, value, timingFunction }) => {
+					let result = value;
+					result = replaceTimingFunction(result, timingFunction);
+					result = replaceAnimationName(result, name);
+					return result;
+				});
+
+				return [
+					...animations.flatMap((animation) => keyframes[animation.name]),
+					{
+						animation: enumerateArrays(results).map((array) => array.join()),
+					},
+				];
+			},
+		},
+		{ values: theme("animation") }
 	);
 }
 
 module.exports = plugin(animationPlugin, defaultOptions);
+
+function enumerateArrays(arr) {
+	let results = [];
+
+	function helper(subArr, index) {
+		if (index === arr.length) {
+			results.push(subArr);
+			return;
+		}
+
+		if (typeof arr[index] === "string") {
+			helper([...subArr, arr[index]], index + 1);
+		} else if (Array.isArray(arr[index])) {
+			for (let item of arr[index]) {
+				helper([...subArr, item], index + 1);
+			}
+		}
+	}
+
+	helper([], 0);
+	return results;
+}
+
+function parseAnimationValue(input, timings = []) {
+	let animations = input.split(COMMA);
+	return animations.map((animation) => {
+		let value = animation.trim();
+		let result = { value };
+		let parts = value.split(SPACE);
+		let seen = new Set();
+
+		for (let part of parts) {
+			if (!seen.has("DIRECTIONS") && DIRECTIONS.has(part)) {
+				result.direction = part;
+				seen.add("DIRECTIONS");
+			} else if (!seen.has("PLAY_STATES") && PLAY_STATES.has(part)) {
+				result.playState = part;
+				seen.add("PLAY_STATES");
+			} else if (!seen.has("FILL_MODES") && FILL_MODES.has(part)) {
+				result.fillMode = part;
+				seen.add("FILL_MODES");
+			} else if (!seen.has("ITERATION_COUNTS") && (ITERATION_COUNTS.has(part) || DIGIT.test(part))) {
+				result.iterationCount = part;
+				seen.add("ITERATION_COUNTS");
+			} else if (!seen.has("TIMING_FUNCTION") && (TIMINGS.has(part) || timings.includes(part))) {
+				result.timingFunction = part;
+				seen.add("TIMING_FUNCTION");
+			} else if (!seen.has("TIMING_FUNCTION") && TIMING_FNS.some((f) => part.startsWith(`${f}(`))) {
+				result.timingFunction = part;
+				seen.add("TIMING_FUNCTION");
+			} else if (!seen.has("DURATION") && TIME.test(part)) {
+				result.duration = part;
+				seen.add("DURATION");
+			} else if (!seen.has("DELAY") && TIME.test(part)) {
+				result.delay = part;
+				seen.add("DELAY");
+			} else if (!seen.has("NAME")) {
+				result.name = part;
+				seen.add("NAME");
+			} else {
+				if (!result.unknown) result.unknown = [];
+				result.unknown.push(part);
+			}
+		}
+
+		return result;
+	});
+}
